1e819073212df95766b7eab7da7515daad1afd61
lacsar712 <lacsar712@users.noreply.github.com>
2026-08-23 23:01:35 +0800

    red: acceptance test against unfixed code

---
+++ internal/store/snapshot.go
@@ -0,0 +1,20 @@
+package store
+
+import "github.com/lacsar712/steamboil/internal/model"
+
+type DrumSnapshotView struct {
+	UnitID   string
+	Drum     model.DrumReading
+	Alarms   []model.AlarmEvent
+	Revision uint64
+}
+
+func CloneDrumSnapshot(s model.PlantSnapshot) DrumSnapshotView {
+	out := DrumSnapshotView{
+		UnitID:   s.UnitID,
+		Drum:     s.Drum,
+		Revision: s.Revision,
+	}
+	out.Alarms = s.Alarms[:len(s.Alarms):len(s.Alarms)]
+	return out
+}
