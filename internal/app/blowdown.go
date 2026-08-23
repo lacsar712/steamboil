ab60ef145568c0f9de3dfcd220b72b504976fd8f
lacsar712 <lacsar712@users.noreply.github.com>
2026-08-23 22:08:51 +0800

    init: steamboil

---
+++ internal/app/blowdown.go
@@ -0,0 +1,39 @@
+package app
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/lacsar712/steamboil/internal/model"
+)
+
+const maxBlowdownOpeningPct = 100.0
+
+func (a *App) OpenBlowdown(ctx context.Context, holder string, openingPct float64) error {
+	_ = holder
+	select {
+	case <-ctx.Done():
+		return fmt.Errorf("%w", model.ErrContextDone)
+	default:
+	}
+	if openingPct >= maxBlowdownOpeningPct {
+		return fmt.Errorf("blowdown: %w", model.ErrBlowdownLimit)
+	}
+	return nil
+}
+
+func (a *App) BlowdownAfterShutdown(ctx context.Context, openingPct float64) error {
+	select {
+	case <-ctx.Done():
+		return fmt.Errorf("%w", model.ErrContextDone)
+	default:
+	}
+	snap := a.Snapshot()
+	if snap.State != model.StateTrip && snap.State != model.StateColdStandby {
+		return fmt.Errorf("plant not shut down")
+	}
+	if openingPct >= maxBlowdownOpeningPct {
+		return fmt.Errorf("blowdown: %w", model.ErrBlowdownLimit)
+	}
+	return nil
+}
